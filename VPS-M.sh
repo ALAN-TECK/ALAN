d0231d78b056e1ee708b3e04a412a2172ce042e9
ALAN-TECK <81272192+ALAN-TECK@users.noreply.github.com>
2021-03-28 12:32:08 -0500

    Update and rename VPS-MX.sh to VPS-M.sh

--- VPS-M.sh
+++ VPS-M.sh
@@ -9,9 +9,7 @@ SCPidioma="${SCPdir}/idioma"
 SCPusr="${SCPdir}/controlador"
 SCPfrm="${SCPdir}/herramientas"
 SCPinst="${SCPdir}/protocolos"
-kalix1="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FMQU4tVEVDSy9BTEFOL21haW4vREFO
-WQ==
-"
+kalix1="aHR0cHM6Ly9naXRodWIuY29tL0FMQU4tVEVDSy9BTEFOL2Jsb2IvbWFpbi9EQU5Z"
 PUTO='base64 -d'
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /etc/resolv.conf
