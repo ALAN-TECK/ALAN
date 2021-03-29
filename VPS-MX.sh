68b9340477cf322c793f35d45913e3147a8e4405
ALAN-TECK <81272192+ALAN-TECK@users.noreply.github.com>
2021-03-28 20:06:55 -0500

    Update and rename VPS-MM.sh to VPS-MX.sh

--- VPS-MX.sh
+++ VPS-MX.sh
@@ -274,11 +274,10 @@ echo "exit 0" >> /etc/rc.local
 /bin/cp /etc/skel/.bashrc ~/
 echo 'clear' >> .bashrc
 echo 'echo ""' >> .bashrc
-echo 'echo -e "\t\033[91m __     ______  ____        __  ____  __ " '>> .bashrc
-echo 'echo -e "\t\033[91m \ \   / /  _ \/ ___|      |  \/  \ \/ / " '>> .bashrc
-echo 'echo -e "\t\033[91m  \ \ / /| |_) \___ \ _____| |\/| |\  /  " '>> .bashrc
-echo 'echo -e "\t\033[91m   \ V / |  __/ ___) |_____| |  | |/  \  " '>> .bashrc
-echo 'echo -e "\t\033[91m    \_/  |_|   |____/      |_|  |_/_/\_\ " '>> .bashrc
+echo 'echo -e "\033[91m      _    _       _    _  _   _____  ___  ___  _  __ " '>> .bashrc
+echo 'echo -e "\033[91m     /_\  | |     /_\  | \| | |_   _|| __|/ __|| |/ / " '>> .bashrc
+echo 'echo -e "\033[91m    / _ \ | |__  / _ \ |  o |   | |  | _|| (__ |   c  " '>> .bashrc
+echo 'echo -e "\033[91m   /_/ \_\/____ /_/ \_\N _\__    __  A___M\___ A_l\_\ " '>> .bashrc
 echo 'echo "" '>> .bashrc
 echo 'mess1="$(less /etc/VPS-MX/message.txt)" ' >> .bashrc
 echo 'echo "" '>> .bashrc
